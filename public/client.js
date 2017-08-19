d61b01f92ca66664948e55644a9ff40b1d28eefd
Tom Ritland <t_ritland@yahoo.com>
2017-08-19 13:31:39 -0500

    GET and POST requests work and input fields clear

--- public/client.js
+++ public/client.js
@@ -1,1 +1,34 @@
-console.log('client.js has been loaded');
+var app = angular.module('EmployeeApp', []);
+
+app.controller('EmployeeController', ['$http', function($http){
+    var self = this;
+    self.employees = [];
+
+
+self.getEmployees = function(){
+    $http({
+        method: 'GET',
+        url: '/employee',
+    }).then(function(response){
+        console.log(response);
+        console.log(response.data);
+        self.employees = response.data;
+        self.newEmployee = {};
+    });
+};
+
+self.postEmployee = function(){
+    $http({
+        method: 'POST',
+        url: '/employee',
+        data: self.newEmployee,
+    }).then(function(response){
+        console.log(response);
+        self.getEmployees();
+        
+    });
+};
+
+    self.getEmployees();
+
+    }]);
--- server/routes/employee.js
+++ server/routes/employee.js
@@ -2,4 +2,45 @@ var express = require('express');
 var router = express.Router();
 var pool = require('../modules/pool');
 
+router.get('/', function(req, res) {
+	console.log('employee GET was hit!');
+	pool.connect(function(errorConnectingToDatabase, client, done){
+		if(errorConnectingToDatabase) {
+			console.log('Error connecting to database', errorConnectingToDatabase);
+			res.sendStatus(500);
+		} else {
+			client.query('SELECT * FROM employees;', function(errorMakingQuery, result) {
+				done();
+				if(errorMakingQuery) {
+					console.log('Error making database query', errorMakingQuery);
+					res.sendStatus(500);
+				} else {
+					res.send(result.rows);
+				}
+			});
+		}
+	});
+});
+
+router.post('/', function(req, res){
+	console.log('employee POST was hit!');
+	pool.connect(function(errorConnectingToDatabase, client, done){
+		if(errorConnectingToDatabase) {
+			console.log('Error connecting to database', errorConnectingToDatabase);
+			res.sendStatus(500);
+		} else {
+            client.query('INSERT INTO employees (first_name, last_name, job_title, salary) VALUES ($1, $2, $3, $4);', 
+            [req.body.firstName, req.body.lastName, req.body.jobTitle, req.body.salary], function(errorMakingQuery, result) {
+				done();
+				if(errorMakingQuery) {
+					console.log('Error making database query', errorMakingQuery);
+					res.sendStatus(500);
+				} else {
+					res.sendStatus(201);
+				}
+			});
+		};
+	});
+});
+
 module.exports = router;
